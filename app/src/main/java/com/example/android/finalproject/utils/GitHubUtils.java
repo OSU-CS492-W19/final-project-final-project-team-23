a68c41bec4f62e2f78bc8b0b5fd379df2c061175
James Luo <luoja@oregonstate.edu>
2019-03-20 15:38:56 -0700

    Refactored some code to start with

---
+++ app/src/main/java/com/example/android/finalproject/utils/GitHubUtils.java
@@ -0,0 +1,39 @@
+package com.example.android.finalproject.utils;
+
+import android.net.Uri;
+
+import com.example.android.finalproject.data.GitHubRepo;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+public class GitHubUtils {
+    private final static String GITHUB_SEARCH_BASE_URL = "https://api.github.com/search/repositories";
+    private final static String GITHUB_SEARCH_QUERY_PARAM = "q";
+    private final static String GITHUB_SEARCH_SORT_PARAM = "sort";
+    private final static String GITHUB_SEARCH_SORT_VALUE = "stars";
+
+    public static final String EXTRA_GITHUB_REPO = "GitHubUtils.GitHubRepo";
+
+    public static class GitHubSearchResults {
+        public ArrayList<GitHubRepo> items;
+    }
+
+    public static String buildGitHubSearchURL(String query) {
+        return Uri.parse(GITHUB_SEARCH_BASE_URL).buildUpon()
+                .appendQueryParameter(GITHUB_SEARCH_QUERY_PARAM, query)
+                .appendQueryParameter(GITHUB_SEARCH_SORT_PARAM, GITHUB_SEARCH_SORT_VALUE)
+                .build()
+                .toString();
+    }
+
+    public static ArrayList<GitHubRepo> parseGitHubSearchResults(String json) {
+        Gson gson = new Gson();
+        GitHubSearchResults results = gson.fromJson(json, GitHubSearchResults.class);
+        if (results != null && results.items != null) {
+            return results.items;
+        } else {
+            return null;
+        }
+    }
+}
